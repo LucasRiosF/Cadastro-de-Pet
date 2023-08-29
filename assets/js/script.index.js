c4f0bcd6419ec439fd99a8d52d01d36154a69f17
Lucas Rios <lucas.rios@aluno.senai.br>
2023-08-29 08:44:14 -0300

    feat: function cadastrarPet

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,5 +1,3 @@
-console.log("Hello, World!");
-
 function verificaInput(){
     let tutor = document.getElementById("input-tutor").value;
     let nome = document.getElementById("input-nome").value;
@@ -13,5 +11,51 @@ function verificaInput(){
     } else {
         console.log("Os dados nao estao em branco");
         return false;
+    }
+}
+
+function mensagem(mensagem, tipo) {
+    let mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.innerHTML = "";
+
+    let tela = `
+    <p class='${tipo}'>${mensagem}</p>
+    `
+
+    mensagemDiv.innerHTML += tela;
+
+    setTimeout (function() {
+        mensagemDiv.innerHTML = "";
+    }, 4000);
+}
+
+class pet{
+    constructor(tutor, nome, especie, foto, nascimento){
+        this.tutor = tutor;
+        this.nome = nome;
+        this.especie = especie;
+        this.foto = foto;
+        this.nascimento = nascimento;
+    }
+}
+
+function cadastrarPet(){
+    let tutor = document.getElementById("input-tutor").value;
+    let nome = document.getElementById("input-nome").value;
+    let especie = document.getElementById("input-especie").value;
+    let foto = document.getElementById("input-foto").value;
+    let nascimento = document.getElementById("input-nascimento").value;
+
+    const pet = new Pet(tutor, nome, especie, foto, nascimento);
+    console.log(pet);
+
+    bibliotecaPets.add(pet);
+
+    renderizarConteudo();
+}
+
+class ListaPet {
+    constructor(){
+        this.ListaPetsArray = [];
     }
 }
